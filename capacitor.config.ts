29b0d53d6e62920653cfc112c69794d4bdc17cca
savindu-st <savindu606@gmail.com>
2026-02-20 13:35:36 +0530

    add camera access

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.savindu.fitlock',
   appName: 'FitLock',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true,
+  },
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
