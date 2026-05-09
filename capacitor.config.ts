cf4b8b42fc667dcf3d8a3d7d710145007f6dac75
savindu-st <savindu606@gmail.com>
2026-05-09 17:02:47 +0530

    feat: configure Android app monitoring services, permissions, and production AdMob ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,7 +13,7 @@ const config: CapacitorConfig = {
   },
   plugins: {
     AdMob: {
-      appId: 'ca-app-pub-3940256099942544~3347511713',
+      appId: 'ca-app-pub-8224368007922953~6404719953',
     }
   }
 };
